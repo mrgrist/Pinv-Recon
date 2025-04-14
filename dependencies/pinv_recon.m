9ea173c455b8e0bb38a0cd7d5f73cb3e54876b7d
univ39 <univ5342@ox.ac.uk>
2025-04-14 16:33:29 +0100

    Add files via upload

--- dependencies/pinv_recon.m
+++ dependencies/pinv_recon.m
@@ -1,4 +1,4 @@
-function [image,imageabs,Recon]=pinv_recon(data,wf,Recon,mtx_reco,B0Map,svd_threshold,plt_svd)
+function [image,imageabs,Recon]=pinv_recon(data,wf,Recon,mtx_reco,B0Map,thresh)
 %% Reconstruction with pseudoinverse of encoding matrix
 % Provide waveform to calculate recon matrix or feed in recon matrix.
 %
@@ -10,13 +10,12 @@ function [image,imageabs,Recon]=pinv_recon(data,wf,Recon,mtx_reco,B0Map,svd_thre
 %         mtx_reco  matrix size after reconstruction (assumed square)  ([])
 %            B0Map  B0 map for off-resonance frequency correction      ([])
 %                   (x,y,z)
-%    svd_threshold  threshold for pinv                               (0.95)
-%          plt_svd  plotting svd and threshold value                    (0)
+%           thresh  threshold for tikhonov regularization            (0.05)
 % Outputs
 %            image  complex image
 %         imageabs  absolute image with coil combination
 %                F  reconstruction matrix
-% 08/2024   Kylie Yeung
+% 08/2024   (anonymized)
 
 if (nargin<1), help(mfilename); return; end
 
@@ -25,16 +24,14 @@ if ~exist('wf','var');wf=[];end
 if ~exist('F','var');Recon=[];end
 if ~exist('mtx_reco','var');mtx_reco=[];end
 if ~exist('B0Map','var');B0Map=[];end
-if ~exist('svd_threshold','var');svd_threshold=[];end
-if isempty(svd_threshold);svd_threshold=0.95;end
-fprintf('Pinv threshold defined as %d%% of cumulative energy \n', svd_threshold*100)
-if ~exist('plt_svd','var');plt_svd=0;end
+if ~exist('thresh','var');thresh=[];end
+if isempty(thresh);thresh=0.05;end
 
 [ncoils,npts,ntimesteps,nslices]=size(data);
 
 %% get reconstruction matrix
 if isempty(Recon)
-    [Recon,pinv_time] = wf2Recon(wf,mtx_reco,B0Map, svd_threshold, plt_svd);
+    [Recon,pinv_time] = wf2Recon(wf,mtx_reco,B0Map,thresh);
 end
 
 %% reconstruct
@@ -55,6 +52,8 @@ else
     image=reshape(data_inverted,[mtx_reco,mtx_reco,nslices,ntimesteps,ncoils]);
 end
 
+image=gather(image);
+
 %% coil combine
 if ncoils>1
     imageabs = sqrt(mean(image.*conj(image),5));
@@ -70,7 +69,7 @@ catch
 end
 
 %% subfunctions
-function [Recon,pinv_time,mtx_acq,mtx_reco] = wf2Recon(wf,mtx_reco,B0Map, svd_threshold, plt_svd)
+function [Recon,pinv_time,mtx_acq,mtx_reco] = wf2Recon(wf,mtx_reco,B0Map, thresh)
 
 k=wf.k;t=wf.t;
 
@@ -100,34 +99,39 @@ if ~isempty(B0Map)
     fprintf('Getting encoding matrix with B0 encoding ... \n')
     nslices=size(B0Map,3);
     Recon=zeros(size(E,2),size(E,1),nslices);
-    svd_time=[];
     for ls=1:nslices
         B0Map_s=-imresize(B0Map(:,:,ls),[mtx_reco mtx_reco]);
         D=exp(1i*2*pi*t(:)*(B0Map_s(:)).');
         E_slice=E.*D;
-        [Recon_s,time_ls]=E2Recon(E_slice,svd_threshold,plt_svd);
-        svd_time=[svd_time time_ls];
+        [Recon_s]=E2Recon(E_slice,thresh);
         Recon(:,:,ls)=Recon_s;
     end
 else
-    [Recon,svd_time]=E2Recon(E,svd_threshold,plt_svd);
-    fprintf('Calculating svd took %f seconds \n',svd_time)
+    [Recon]=E2Recon(E,thresh);
 end
 
 end
 
-function [Recon,svd_time]=E2Recon(E,svd_threshold,plt_svd)
-svd_start=tic; [U,S,V]=svd(E,'econ'); svd_time=toc(svd_start);
-svdE=diag(S);
-if plt_svd;figure;plot(svdE);end
+function [Recon]=E2Recon(E,thresh)
+    if canUseGPU;E=gpuArray(E);end
 
-cumulative_energy = cumsum(svdE) / sum(svdE);
-g = find(cumulative_energy >= svd_threshold, 1);
-if isempty(g);g=size(svdE,1);end
-% imax=svdE(g);
-
-invS=1./svdE; invS(g+1:end)=0;invS=diag(invS);
-tic, Recon=V*invS*U'; toc, 
+    if size(E,1)>size(E,2) %overdetermined
+        lambda=thresh*norm(E,1);
+    
+        EHE=E'*E+lambda*eye(size(E,2));
+    
+        L = chol(EHE, 'lower');
+        L_inv = inv(L);
+        iEHE = L_inv' * L_inv;
+        Recon=iEHE*E';
+    else
+        lambda=thresh*norm(E,1);
+    
+        EEH=E*E'+lambda*eye(size(E,1));
+        L = chol(EEH, 'lower');
+        L_inv = inv(L);
+        iEEH = L_inv' * L_inv;
+        Recon=E'*iEEH;
+    end  
 end
-
 end
